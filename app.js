31f8b3536f970c6324956cdc94d6bc0a1355b728
Yash Arya <yasharya11@gmail.com>
2018-03-27 13:51:01 -0700

    first POC done

--- app.js
+++ app.js
@@ -1,131 +1,104 @@
 // Initialize Firebase
-// Make sure to match the configuration to the script version number in the HTML
-// (Ex. 3.0 != 3.7.0)
 var config = {
-    apiKey: "AIzaSyDhZ3iMAnMySu4Q5DeUATM5_7GhTklFUng",
-    authDomain: "hello-world-71ef1.firebaseapp.com",
-    databaseURL: "https://hello-world-71ef1.firebaseio.com",
-    projectId: "hello-world-71ef1",
-    storageBucket: "hello-world-71ef1.appspot.com",
-    messagingSenderId: "81358045040"
+    apiKey: "AIzaSyACNVOc0XnWfoBbVb5OIchMrJlpOwIpI2c",
+    authDomain: "timesheet-eb837.firebaseapp.com",
+    databaseURL: "https://timesheet-eb837.firebaseio.com",
+    projectId: "timesheet-eb837",
+    storageBucket: "",
+    messagingSenderId: "167125142236"
 };
 firebase.initializeApp(config);
-
-
 // Assign the reference to the database to a variable named 'database'
 var database = firebase.database();
 
-// Initial Values
-var initName = "No One Yet";
-var initRole = "No Roles";
-var initDate = "No Dates Yet";
-var initRate = "No Rates Yet";
-
-var initEmployee = {
-    name: initName,
-    role: initRole,
-    date: initDate,
-    rate: initRate
-};
 
 
-// --------------------------------------------------------------
-
-// At the initial load and subsequent value changes, get a snapshot of the stored data.
-// This function allows you to update your page in real-time when the firebase database changes.
-database.ref().on("value", function (snapshot) {
-
-    // If Firebase has a highPrice and highBidder stored (first case)
-    if (snapshot.child("name").exists() && snapshot.child("role").exists()) {
-
-        console.log(snapshot.child("name"));
-        // // employeeArray = snapshot.val();
-
-        // // var anotherEmp
-
-        // // employeeArray.push(anotherEmp)
-
-        // // database.re.set()
-        // // Set the variables for highBidder/highPrice equal to the stored values in firebase.
-        // highBidder = snapshot.val().highBidder;
-        // highPrice = snapshot.val().highPrice;
+////////////////////////////////////////////////////
 
 
 
-        // // Change the HTML to reflect the stored values
-        // $("#highest-bidder").text(highBidder);
-        // $("#highest-price").text(highPrice);
-
-        // // Print the data to the console.
 
 
-    }
+$("#add-employee").on("click", function () {
+    event.preventDefault();
+    var name = $("#name-input").val().trim();
+    var role = $("#role-input").val().trim();
+    var date = $("#date-input").val().trim();
+    var rate = $("#rate-input").val().trim();
 
-    // Else Firebase doesn't have a highPrice/highBidder, so use the initial local values.
-    else {
-        database.ref().set({
-            initEmloyee: initEmployee
-        });
-        console.log("Database Set");
-        console.log(initEmployee);
-
-        // // Change the HTML to reflect the initial values
-        // $("#highest-bidder").text(highBidder);
-        // $("#highest-price").text(highPrice);
-
-        // // Print the data to the console.
-
-
-    }
+    database.ref().push({
+        name: name,
+        role: role,
+        date: date,
+        rate: rate
+    });
 });
 
 
-  // If any errors are experienced, log them to console.
-// }, function (errorObject) {
-//   console.log("The read failed: " + errorObject.code);
-// });
+database.ref().orderByChild("dataAdded").limitToLast(1).on("child_added", function(snapshot) {
+    var sv = snapshot.val();
+
+    console.log(sv.name);
+    console.log(sv.rate);
+    console.log(sv.date);
+    console.log(sv.role);
+
+    var newRow = $("<tr>");
+    var nameRow = $("<td>");
+    var roleRow = $("<td>");
+    var dateRow = $("<d>");
+    var monthsWorkedRow = $("<td>");
+    var rateRow = $("<td>");
+    var billedRow = $("<td>");
+    
+    nameRow.text(sv.name);
+    roleRow.text(sv.role);
+    dateRow.text(sv.date);
+    monthsWorkedRow.text(" ");
+    rateRow.text(sv.rate);
+    billedRow.text(" ");
+
+    newRow.append(nameRow);
+    newRow.append(roleRow);
+    newRow.append(dateRow);
+    newRow.append(monthsWorkedRow);
+    newRow.append(rateRow);
+    newRow.append(billedRow);
+
+    console.log(newRow);
+
+    $("tbody").append(newRow);
+
+
+
+
+
+
+});
+
 
 // // --------------------------------------------------------------
 
-// // Whenever a user clicks the submit-bid button
-// $("#submit-bid").on("click", function (event) {
-//   // Prevent form from submitting
-//   event.preventDefault();
+// // At the initial load and subsequent value changes, get a snapshot of the stored data.
+// // This function allows you to update your page in real-time when the firebase database changes.
+// database.ref().on("value", function (snapshot) {
+//     if (snapshot.child("name").exists() && snapshot.child("role").exists()) {
 
-//   // Get the input values
-//   var bidderPrice = $("#bidder-price").val().trim();
-//   var bidderName = $("#bidder-name").val().trim();
+//     } else {
+//         // Initial Values
+//         var initName = "No One Yet";
+//         var initRole = "No Roles";
+//         var initDate = "No Dates Yet";
+//         var initRate = "No Rates Yet";
 
-//   console.log(bidderPrice);
-//   console.log(highPrice);
-
-//   // Log the Bidder and Price (Even if not the highest)
-//   if (bidderPrice > highPrice) {
-
-//     // Alert
-//     alert("You are now the highest bidder.");
-
-//     // Save the new price in Firebase
-//     database.ref().set({
-//       highPrice: bidderPrice,
-//       highBidder: bidderName
+//         var initEmployee = {
+//             name: initName,
+//             role: initRole,
+//             date: initDate,
+//             rate: initRate
+//         };
+//     }
+//     database.ref().push({
+//         employee: initEmployee
 //     });
-
-//     // Log the new High Price
-
-
-//     // Store the new high price and bidder name as a local variable
-//     highBidder = bidderName;
-//     highPrice = bidderPrice;
-
-//     // Change the HTML to reflect the new high price and bidder
-//     $("#highest-bidder").text(highBidder);
-//     $("#highest-price").text(highPrice);
-//   }
-
-//   else {
-//     // Alert
-//     alert("Sorry that bid is too low. Try again.");
-//   }
-
 // });
